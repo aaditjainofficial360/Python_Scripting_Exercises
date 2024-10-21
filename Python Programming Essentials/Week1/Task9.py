9a49320e5bd46bec555b2b0028c9bf31e5524d8a
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-22 01:19:09 +0530

    Done

---
+++ Python Programming Essentials/Week1/Task9.py
@@ -0,0 +1,16 @@
+"""
+Template - Compute the statement about a person's name and age, given the person's name and age.
+"""
+###################################################
+# Name and age formula
+# Student should enter statement on the next line.
+age = 56
+name = "Joe Warren"
+
+statement = name+" "+"is"+" "+str(age)+" "+"years old."
+print(statement)
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 56 years old.
