796e7de88fda15bd96c28bf790cad13b2dcc0987
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-22 01:23:25 +0530

    Done

---
+++ Python Programming Essentials/Week1/Task10.py
@@ -0,0 +1,22 @@
+"""
+Template - Compute the distance between the points (x0, y0) and (x1, y1).
+"""
+
+###################################################
+# Distance formula
+# Student should enter statement on the next line.
+
+# Hint: You need to use the power operation ** .
+x_1=2
+y_1=2
+x_2=5
+y_2=6
+
+distance_formula = ((y_2-y_1)**2+(x_2-x_1)**2)**(0.5)
+
+print(distance_formula)
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#5.0
